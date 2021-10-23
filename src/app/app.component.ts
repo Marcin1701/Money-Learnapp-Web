c75544861f47b2e4b834faaa91689192a8f774ed
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-10-23 13:38:25 +0200

    Connect Angular to Spring Backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StudentService} from "./services/student.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'money-sandbox-web';
+export class AppComponent implements OnInit {
+
+  students: any;
+
+  constructor(private studentService: StudentService) {
+  }
+
+  ngOnInit(): void {
+   this.studentService.getStudents().subscribe(data => this.students = data);
+  }
 }
